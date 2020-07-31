edbeb3e0847435f760eb1338463543ab8b65d2cc
github username <github email address>
2020-07-30 23:12:24 -0400

    add controller actions for categories and costumes

---
+++ app/controllers/api/categories_controller.rb
@@ -0,0 +1,20 @@
+class Api::V1::CategoriesController < ApplicationController
+    before_action :set_costume
+    
+    def index
+        categories = @costume.categories
+        render json: categories
+    end
+
+    def show
+        category = Category.find(params[:id])
+        render json: category
+    end
+
+private
+
+    def set_costume
+        @costume = Costume.find(params[:costume_id])
+    end
+
+end
---
+++ app/controllers/api/costumes_controller.rb
@@ -0,0 +1,30 @@
+class Api::V1::CostumesController < ApplicationController
+    def index
+        costumes = Costume.all 
+        render json: costumes
+    end
+
+    def create
+        costume = Costume.new(costume_params)
+        if costume.save
+            render json: costume
+        else
+            render json: {error: "Invalid Costume"}
+    end
+
+    def show
+        costume = Costume.find(params[:id])
+        render json: costume 
+    end
+
+    def destroy
+        costume = Costume.find(params[:id])
+        costume.destroy
+    end
+
+    private
+    def costume_params
+        params.require(:costume).permit(:title, :price, :owner_name, :owner_email, :location, :decription, :photos)
+    end
+
+end

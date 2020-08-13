22a9444768d10a0fea9c9dc513719635bdd78529
github username <github email address>
2020-08-13 00:16:29 -0400

    change photo to avatar to rectify naming conventions

--- app/models/costume.rb
+++ app/models/costume.rb
@@ -1,5 +1,6 @@
 class Costume < ApplicationRecord
     belongs_to :category
+    has_one_attached :avatar
 
     # vaildates :title, :owner_name, :location, presence: true
     # vaildates :owner_email, presence: true 

b2217202a8cd9e7bcf06efd899c8d5a5c285a8bf
Deanna Kelley <deanna.georges@gmail.com>
2020-08-19 21:23:31 -0400

    add and remove activestorage routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
+      resources :costumes 
       resources :categories
-      resources :costumes
+      get '/costumes/categories/:id', to: 'costumes#index'
+      # post 'rails/active_storage/direct_uploads', to: 'direct_uploads#create'
     end
   end
 
